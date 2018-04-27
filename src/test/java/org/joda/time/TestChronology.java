3774d091bca3915a196827461a91127c5dc7903b
Alex Wyllie <alex@dilex.net>
2018-04-27 12:09:55 -0400

    Even more broken now

--- src/test/java/org/joda/time/TestChronology.java
+++ src/test/java/org/joda/time/TestChronology.java
@@ -140,7 +140,7 @@ public class TestChronology extends TestCase {
         Chronology chrono2 = LenientChronology.getInstance(ISOChronology.getInstanceUTC());
         Chronology chrono3 = LenientChronology.getInstance(ISOChronology.getInstance());
         
-        assertEquals(true, chrono1.equals(chrono2));
+        assertEquals(false, chrono1.equals(chrono2));
         assertEquals(false, chrono1.equals(chrono3));
         
         DateTime dt1 = new DateTime(0L, chrono1);
